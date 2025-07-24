77d2dd37306fffc48c76fef029957b93bd2d223c
ayoubbijarche <ayoubbijarche6@gmail.com>
2025-07-24 18:52:23 +0100

    some changes

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,37 @@
-use std::{io::stdin, process::Command};
+use std::{env, io::{stdin, stdout, Write}, path::Path, process::Command};
 
 
 fn main() {
-    let mut input = String::new();
+    loop{
+        print!("el_shell>> ");
+        stdout().flush();
+        let mut input = String::new();
         stdin().read_line(&mut input).unwrap();
-    
-        // read_line leaves a trailing newline, which trim removes
-        let command = input.trim(); 
-    
-        Command::new(command)
-            .spawn()
-            .unwrap();
+        let mut parts = input.trim().split_whitespace();
+        let command = parts.next().unwrap();
+        let args = parts;
+        
+
+        match command {
+                   "cd" => {
+                       let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                       let root = Path::new(new_dir);
+                       if let Err(e) = env::set_current_dir(&root) {
+                           eprintln!("{}", e);
+                       }
+                   },
+                   "exit" => return,
+                   command => {
+                       let child = Command::new(command)
+                           .args(args)
+                           .spawn();
+       
+                       match child {
+                           Ok(mut child) => { child.wait(); },
+                           Err(e) => eprintln!("{}", e),
+                       };
+                   }
+               }
+               
+    }
 }
